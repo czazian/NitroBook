91e6666164b1fdc31fc7dc51f76b45b6f242f94a
ささやかな風と青い海 <99159488+czazian@users.noreply.github.com>
2023-12-05 23:23:02 +0800

    Minor fix for login

--- AssignmentWAD/Customer/customerLogin.aspx.designer.cs
+++ AssignmentWAD/Customer/customerLogin.aspx.designer.cs
@@ -42,13 +42,13 @@ namespace AssignmentWAD.Customer
         protected global::System.Web.UI.WebControls.HyperLink hrefForgotPass;
 
         /// <summary>
-        /// hrefRegister control.
+        /// btnRegister control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink hrefRegister;
+        protected global::System.Web.UI.WebControls.Button btnRegister;
 
         /// <summary>
         /// btnLogin control.
