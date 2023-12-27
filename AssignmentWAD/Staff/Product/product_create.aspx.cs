b60d691b2ca7706b9cf79d2462e226b40054adc6
LeonardDunKnow <leonardloh08@gmail.com>
2023-12-27 13:45:59 +0800

    update Staff product module
    CRUD in progress

--- AssignmentWAD/Staff/Product/product_create.aspx.cs
+++ AssignmentWAD/Staff/Product/product_create.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,11 +12,51 @@ namespace AssignmentWAD.Staff.Product
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
         }
 
         protected void btnBack_Click(object sender, EventArgs e)
         {
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            SqlConnection cnn;
+
+            connetionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\xampp\htdocs\Web-Application-Development\AssignmentWAD\App_Data\NitroBooks.mdf;Integrated Security=True";
+
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+
+            String sql = "";
+
+            sql = "INSERT INTO Book(Title, Description, Author, Price, Category, Quantity, Image, Language) values (@title, @desc, @author, @price, @cate, @quantity, @image, @language)";
+            
+            SqlCommand cmdInsert = new SqlCommand(sql, cnn);
+
+            cmdInsert.Parameters.AddWithValue("@title", txtTitle.Text);
+            cmdInsert.Parameters.AddWithValue("@desc", txtDesc.Text);
+            cmdInsert.Parameters.AddWithValue("@author", txtAuthor.Text);
+            cmdInsert.Parameters.AddWithValue("@price", txtPrice.Text);
+            cmdInsert.Parameters.AddWithValue("@cate", ddlProductCategory.Text);
+            cmdInsert.Parameters.AddWithValue("@quantity", txtQuantity.Text);
+            cmdInsert.Parameters.AddWithValue("@image", newImgProd.FileName.ToString());
+            cmdInsert.Parameters.AddWithValue("@language", ddlLanguage.Text);
+
+            int i = cmdInsert.ExecuteNonQuery();
+            if (i > 0)
+            {
+                Response.Redirect("~/Staff/Product/product.aspx");
+            }
+            else
+            {
+                Response.Write("Fail to add!");
+            }
+
+
+            cmdInsert.Dispose();
+            cnn.Close();
+            
         }
     }
 }
--- AssignmentWAD/Staff/Product/product_edit.aspx.cs
+++ AssignmentWAD/Staff/Product/product_edit.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,41 @@ namespace AssignmentWAD.Staff.Product
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string connetionString;
+            SqlConnection cnn;
 
+            connetionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\xampp\htdocs\Web-Application-Development\AssignmentWAD\App_Data\NitroBooks.mdf;Integrated Security=True";
+
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+
+            String sql = "SELECT * FROM Book WHERE BookID=@id";
+
+            SqlCommand cmdRetrieve = new SqlCommand(sql, cnn);
+
+            cmdRetrieve.Parameters.AddWithValue("@id", Request.QueryString["bookID"]);
+            SqlDataReader book = cmdRetrieve.ExecuteReader();
+
+            if (book.HasRows && book.Read())
+            {
+                //work, can get query string
+                txtTitle.Text = book.GetValue(1).ToString();
+                txtDesc.Text = book.GetValue(2).ToString();
+                txtAuthor.Text = book.GetValue(3).ToString();
+                txtPrice.Text = book.GetValue(4).ToString();
+                ddlProductCategory.SelectedValue = book.GetValue(5).ToString();
+                txtQuantity.Text = book.GetValue(6).ToString();
+                image.ImageUrl = book.GetValue(7).ToString();
+                ddlLanguage.SelectedValue = book.GetValue(8).ToString();
+
+            }
+            else
+            {
+                Response.Write("Invalid Records");
+            }
+
+            cmdRetrieve.Dispose();
+            cnn.Close();
         }
     }
 }
